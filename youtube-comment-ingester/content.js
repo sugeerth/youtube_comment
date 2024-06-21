4fdd4b33ce11957b23a40413ee15d081dabbc02e
Sugeerth Murugesan <sugeerth@gmail.com>
2024-06-20 17:20:15 -0700

    modulairze

--- youtube-comment-ingester/content.js
+++ youtube-comment-ingester/content.js
@@ -1,197 +1,135 @@
-/* 
-Various sections of YouTube are loaded dynamically.
-The code below uses a mutation observer to listen to
-DOM changes and check if the comments have been
-loaded on the page.
-*/
-chrome.runtime.onMessage.addListener((request) => {
-  if (!request.activate) return;
+console.log('%c🧑‍💻 YT Comments Crawler: Extension loaded.', 'background-color: lightblue;');
 
-  const trackedElement = document.getElementById('content');
-  const config = {
-    childList: true,
-    subtree: true,
-  };
+// Utility Functions
+const Utils = (() => {
+  const getVideoId = () => window.location.search.split('v=')[1].split(/[&#]/)[0];
+  const getVideoTitle = () => document.title.replace(' - YouTube', '').trim().replace(/[^ \p{L}0-9-_.]/gu, '').replace(/\s+/g, ' ');
 
-  const callback = (mutationList, observer) => {
-    if (mutationList.some((mutation) => mutation.target.id === 'comments')) {
-      activateExtension();
-      observer.disconnect();
+  const scrollToBottom = async () => {
+    let lastHeight = 0;
+    while (true) {
+      window.scrollTo(0, document.documentElement.scrollHeight);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      const newHeight = document.documentElement.scrollHeight;
+      if (newHeight === lastHeight) break;
+      lastHeight = newHeight;
     }
   };
 
-  const observer = new MutationObserver(callback);
-  observer.observe(trackedElement, config);
-});
+  const getSortableLikes = likes => {
+    let multiplier = 1;
+    if (likes.endsWith("K")) multiplier = 1000;
+    if (likes.endsWith("M")) multiplier = 1000000;
+    return parseFloat(likes) * multiplier;
+  };
 
-/*
-Goes through the comments, determines if the number of lines
-is more than 4, and displays the 'Read More' button
-to expand the comments.
-*/
+  return { getVideoId, getVideoTitle, scrollToBottom, getSortableLikes };
+})();
 
-function expandComments(commentsEl) {
-  const commentTextContainer = commentsEl.querySelectorAll(
-    '#expander.style-scope.ytd-comment-renderer'
-  );
-
-  const lineHeight =
-    commentTextContainer.length > 0
-      ? Number.parseFloat(
-          getComputedStyle(
-            commentTextContainer[0].querySelector('#content-text')
-          ).lineHeight
-        )
-      : 20;
-
-  function showExpandButton(comment, shouldShow) {
-    const btnMore = comment.querySelector('#more');
-    const btnLess = comment.querySelector('#less');
-
-    if (!shouldShow) {
-      btnMore.setAttribute('hidden', '');
-      btnLess.setAttribute('hidden', '');
-      return;
-    }
-
-    btnLess.setAttribute('hidden', '');
-    btnMore.removeAttribute('hidden');
-  }
-
-  commentTextContainer.forEach((comment) => {
-    comment.querySelector('#content').offsetHeight;
-    const textContainerHeight =
-      comment.querySelector('#content-text').offsetHeight;
-    const lines = Math.ceil(textContainerHeight / lineHeight);
-    showExpandButton(comment, lines > 4);
-  });
-}
-
-/*
-Save current extension position locally
-*/
-
-function savePosition(position) {
-  chrome.storage.local.set({ comments_placement: position });
-}
-
-/*
-Gathers info from the page, like the theme and DOM Tree.
-A button is then added to the comments section to toggle between
-default view and sidebar view, and event listeners are attached.
-*/
-
-function activateExtension() {
-  const commentsEl = document.querySelector('#comments');
-  const page = document.querySelector('html');
-  const player = document.querySelector('.video-stream.html5-main-video');
-  const originalCommentsContainer = document.querySelector('#below');
-  const sidebar = document.querySelector('#secondary-inner');
-  const videoSizeButton = document.querySelector('.ytp-size-button');
-
-  let boolTheaterMode = videoSizeButton
-    .getAttribute('data-title-no-tooltip')
-    .includes('Default');
-
-  const isDark = page.hasAttribute('dark');
-  commentsEl.classList.add('extension-control');
-
-  const popButton = document.createElement('button');
-  popButton.classList.add('comments-header-btn');
-
-  function defaultView() {
-    commentsEl.style.display = 'none';
-    commentsEl.classList.remove('popout', 'dark-mode', 'light-mode');
-    commentsEl.style.height = 'auto';
-
-    popButton.removeEventListener('click', defaultView);
-    popButton.addEventListener('click', sidebarView);
-
-    popButton.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="comments-icon ${
-        isDark ? 'stroke-light' : 'stroke-dark'
-      }">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-      </svg>`;
-
-    originalCommentsContainer.append(commentsEl);
-    commentsEl.style.display = 'block';
-
-    savePosition('default');
-  }
-
-  function sidebarView() {
-    if (boolTheaterMode) {
-      videoSizeButton.click();
-    }
-    commentsEl.classList.add('popout', isDark ? 'dark-mode' : 'light-mode');
-    setTimeout(() => {
-      commentsEl.style.height = `${player.offsetHeight}px`;
-    }, 0);
-    popButton.removeEventListener('click', sidebarView);
-    popButton.addEventListener('click', () => {
-      defaultView();
-      expandComments(commentsEl);
-      commentsEl.scrollIntoView({ behavior: 'smooth' });
+// Comment Functions
+const Comments = (() => {
+  const extractComments = () => {
+    const commentElements = document.querySelectorAll('#contents #content-text');
+    return Array.from(commentElements).map(commentElement => {
+      const commentBody = commentElement.closest('#comment');
+      return {
+        text: commentElement.textContent.trim().replace(/\n/g, ' ').replace(/"/g, '""').replace(/'/g, "''"),
+        author: commentBody.querySelector('#author-text').textContent.trim().replace(/"/g, '""').replace(/'/g, "''"),
+        likes: commentBody.querySelector('#vote-count-middle').textContent.trim().replace(/"/g, '""').replace(/'/g, "''")
+      };
     });
+  };
 
-    popButton.innerHTML = `
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="comments-icon ${
-      isDark ? 'stroke-light' : 'stroke-dark'
-    }">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"" />
-    </svg>`;
+  const sortComments = comments => comments.sort((a, b) => Utils.getSortableLikes(b.likes) - Utils.getSortableLikes(a.likes));
 
-    sidebar.prepend(commentsEl);
-    expandComments(commentsEl);
-    page.scrollIntoView({ behavior: 'smooth' });
+  const convertToCSV = comments => {
+    const headers = ['Text', 'Author', 'Likes'];
+    const rows = comments.map(comment => [comment.text, comment.author, comment.likes]);
+    return [headers, ...rows].map(row => row.map(value => `"${value}"`).join(',')).join('\n');
+  };
 
-    savePosition('sidebar');
-  }
+  const downloadCSV = (csv, fileName) => {
+    const blob = new Blob([csv], { type: 'text/csv' });
+    const link = document.createElement('a');
+    link.download = `${fileName}.csv`;
+    link.href = URL.createObjectURL(blob);
+    link.click();
+  };
 
-  if (!commentsEl.querySelector('header')) {
-    const header = document.createElement('header');
-    header.classList.add('comments-header');
-    header.append(popButton);
-    commentsEl.prepend(header);
-  }
+  return { extractComments, sortComments, convertToCSV, downloadCSV };
+})();
 
-  // Click event listener to handle the 'Read More' and 'Show Less' button clicks.
-  function handleExpandButtonClick(e) {
-    if (
-      !e.target.classList.contains('more-button') &&
-      !e.target.classList.contains('less-button')
-    )
-      return;
+// UI Functions
+const UI = (() => {
+  const createButton = () => {
+    const button = document.createElement('button');
+    button.setAttribute('id', 'btn-crawl-comments');
+    button.innerText = '⏬';
+    button.title = 'Crawl comments';
+    styleButton(button);
+    document.body.appendChild(button);
+    return button;
+  };
 
-    const commentContainer = e.target.closest(
-      '#expander.style-scope.ytd-comment-renderer'
-    );
-    const btnMore = commentContainer.querySelector('#more');
-    const btnLess = commentContainer.querySelector('#less');
+  const styleButton = button => {
+    Object.assign(button.style, {
+      position: 'fixed', opacity: 0.7, bottom: 0, left: 0,
+      background: 'var(--yt-spec-brand-background-secondary)',
+      color: 'var(--yt-spec-icon-active-other)', border: '1px solid var(--yt-spec-brand-background-primary)',
+      borderRadius: '2px', padding: '4px 4px', cursor: 'pointer', fontSize: '2em',
+      transition: 'background 0.2s ease-in-out', textDecoration: 'none'
+    });
+  };
 
-    if (e.target.classList.contains('more-button')) {
-      btnMore.setAttribute('hidden', '');
-      btnLess.removeAttribute('hidden');
-      commentContainer.removeAttribute('collapsed');
+  const toggleButtonVisibility = button => {
+    if (document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement || document.msFullscreenElement) {
+      console.log('%c🧑‍💻 YT Comments Crawler: Went full screen, hiding the button.', 'background-color: lightblue;');
+      button.hidden = true;
     } else {
-      btnMore.removeAttribute('hidden');
-      btnLess.setAttribute('hidden', '');
-      commentContainer.setAttribute('collapsed', '');
+      console.log('%c🧑‍💻 YT Comments Crawler: Exited full screen, showing the button.', 'background-color: lightblue;');
+      button.hidden = false;
     }
-  }
+  };
 
-  videoSizeButton.addEventListener('click', () => {
-    boolTheaterMode = !boolTheaterMode;
+  const addFullscreenListeners = button => {
+    ['fullscreenchange', 'mozfullscreenchange', 'MSFullscreenChange', 'webkitfullscreenchange'].forEach(event =>
+      document.addEventListener(event, () => toggleButtonVisibility(button), false)
+    );
+  };
 
-    if (boolTheaterMode) {
-      defaultView();
-    }
-  });
-  commentsEl.addEventListener('click', handleExpandButtonClick);
+  return { createButton, addFullscreenListeners };
+})();
 
-  chrome.storage.local.get(['comments_placement']).then((data) => {
-    if (data.comments_placement === 'sidebar') sidebarView();
-    else defaultView();
-  });
-}
+// Main Function
+const Main = (() => {
+  const handleButtonClick = async button => {
+    console.log('%c🧑‍💻 YT Comments Crawler: Button clicked.', 'background-color: lightblue;');
+    const videoId = Utils.getVideoId();
+    const videoTitle = Utils.getVideoTitle();
+    console.log(`%c🧑‍💻 YT Comments Crawler: videoId: ${videoId}`, 'background-color: lightblue;');
+    console.log(`%c🧑‍💻 YT Comments Crawler: videoTitle: ${videoTitle}`, 'background-color: lightblue;');
+
+    button.innerText = '⏳';
+    button.title = 'Crawling...';
+
+    await Utils.scrollToBottom();
+    const comments = Comments.extractComments();
+    const sortedComments = Comments.sortComments(comments);
+    const csv = Comments.convertToCSV(sortedComments);
+    Comments.downloadCSV(csv, `${videoTitle} [${videoId}]`);
+
+    button.innerText = '🔃';
+    button.title = 'Crawl again';
+  };
+
+  const init = () => {
+    const button = UI.createButton();
+    UI.addFullscreenListeners(button);
+    button.addEventListener('click', () => handleButtonClick(button));
+  };
+
+  return { init };
+})();
+
+// Initialize the extension
+Main.init();
